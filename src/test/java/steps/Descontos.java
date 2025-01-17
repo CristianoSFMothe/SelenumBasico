ce138891894c8b0d1bab7a1c99341b54806f067e
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-01-17 18:46:14 -0300

    criação do primeiro step

---
+++ src/test/java/steps/Descontos.java
@@ -0,0 +1,28 @@
+package steps;
+
+import io.cucumber.java.pt.Dada;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class Descontos {
+    @Dada("^que estou no site da qazando")
+    public  void acessar_site_qazando() {
+
+    }
+
+    @Quando("^eu preencho meu e-mail")
+    public void preencher_email() {
+
+    }
+
+    @E("^clico em ganhar cupom")
+    public void clicar_ganhar_cupom() {
+
+    }
+
+    @Entao("^eu vejo código de desconto")
+    public void ver_cupom_desconto() {
+
+    }
+}
